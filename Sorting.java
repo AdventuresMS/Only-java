23ac34e0e4ea8e2bfd9fed9e4e513ac8f2065814
Monika kumari <157510932+AdventuresMS@users.noreply.github.com>
2025-02-21 10:46:54 +0000

    Container config

---
+++ Sorting.java
@@ -0,0 +1,22 @@
+public class Sorting{
+    public static void main(String[] args) {
+        int [] arr = {1,3,2,9,5,2};
+        int n = arr.length;
+        for(int ele:arr){
+            System.out.print(ele);
+        }
+        System.out.println();
+
+    for(int i = 0; i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            int temp = arr[i];
+            arr[i]= arr[i+1];
+            arr[i+1] = temp;
+        }
+        
+    }
+    for(int ele:arr){
+        System.err.print(ele);
+    }
+    }
+}
